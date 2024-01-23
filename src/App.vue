f0399bc8ed2be0bdc20479d231dfb506d958d68f
Vitaliy Dimov <v@forij.dev>
2024-01-24 01:28:42 +0200

    auto sacle text

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,6 @@
 
 <template>
   <div class="w-full min-h-screen flex items-center justify-center bg-bg">
-    <h1 class="neonText text-primary hover:cursor-pointer text-8xl"> FORIJ.DEV </h1>
+    <h1 class="neonText text-primary hover:cursor-pointer text-[15vw]"> FORIJ.DEV </h1>
   </div>
 </template>
